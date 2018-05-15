dc157710f12ac10376ac70027ca8291d4f27d5d4
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-05-15 11:04:35 +0900

    #22

--- lib/onionbot/application.rb
+++ lib/onionbot/application.rb
@@ -10,32 +10,32 @@ module OnionBot
       @config = Config.instance
       @logger = Logger.new
       @chinachu = Chinachu.new
-      @slack = Slack.new
       @data_file = DataFile.new
     end
 
     def execute
       @logger.info({message: 'start'})
       sleep(sleep_seconds)
-      @data_file.load.each do |key, queue|
-        next if queues[key]
-        @slack.say(create_message(queue, '録画終了'))
+      @data_file.load.each do |k, q|
+        hooks.map{ |h| h.say(create_message(q, '録画終了'))} unless @chinachu.queues[k]
       end
-      queues.each do |key, queue|
-        next if @data_file.load[key]
-        @slack.say(create_message(queue, '録画開始'))
+      @chinachu.queues.each do |k, q|
+        hooks.map{ |h| h.say(create_message(q, '録画開始'))} unless @data_file.load[k]
       end
-      @data_file.save(queues)
+      @data_file.save(@chinachu.queues)
       @logger.info({message: 'end'})
     rescue => e
-      @slack.say({class: e.class, message: e.message})
+      hooks.map{ |h| h.say({class: e.class, message: e.message})}
       exit 1
     end
 
     private
 
-    def queues
-      return @chinachu.queues
+    def hooks
+      return enum_for(__method__) unless block_given?
+      @config['local']['slack']['hooks'].each do |url|
+        yield Slack.new(url)
+      end
     end
 
     def create_message(queue, message_string)
