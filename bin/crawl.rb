363a5a4f881e4a76592301a391dbdbbd42d7e7e7
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-05-25 05:27:03 +0900

    .

--- bin/crawl.rb
+++ bin/crawl.rb
@@ -4,9 +4,14 @@ path = File.expand_path(__FILE__)
 path = File.expand_path(File.readlink(path)) while File.symlink?(path)
 dir = File.expand_path('../..', path)
 $LOAD_PATH.unshift(File.join(dir, 'app/lib'))
-ENV['BUNDLE_GEMFILE'] ||= File.join(dir, 'Gemfile')
+ENV['BUNDLE_GEMFILE'] = File.join(dir, 'Gemfile')
 
 require 'bundler/setup'
 require 'onionbot'
 
-Onionbot::Crawler.new.crawl
+begin
+  Onionbot::Crawler.new.exec
+rescue => e
+  warn e.message
+  exit 1
+end
