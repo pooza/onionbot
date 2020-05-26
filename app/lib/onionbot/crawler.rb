81d023389105ada838ae770e982789a1b00790d0
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-05-27 06:03:03 +0900

    .

--- app/lib/onionbot/crawler.rb
+++ app/lib/onionbot/crawler.rb
@@ -2,30 +2,21 @@ module Onionbot
   class Crawler
     def initialize
       @config = Config.instance
-      @logger = Logger.new
       @chinachu = Chinachu.new
       @data_file = DataFile.new
     end
 
-    def crawl
-      @logger.info(message: 'start', version: Package.version)
+    def exec
+      messages = []
       sleep(@config['/sleep'])
       @data_file.load.each do |k, q|
-        Slack.broadcast(create_message(q, '録画終了')) unless @chinachu.queues[k]
-        @logger.info(q: q, message: 'end')
+        messages.push(create_message(q, '録画終了'))
       end
       @chinachu.queues.each do |k, q|
-        Slack.broadcast(create_message(q, '録画開始')) unless @data_file.load[k]
-        @logger.info(q: q, message: 'start')
+        messages.push(create_message(q, '録画開始'))
       end
       @data_file.save(@chinachu.queues)
-      @logger.info(message: 'end', version: Package.version)
-    rescue => e
-      e = Ginseng::Error.create(e)
-      e.package = Package.full_name
-      Slack.broadcast(e)
-      @logger.error(e)
-      exit 1
+      puts messages.map {|m| YAML.dump(m)}.join("=====\n")
     end
 
     private
--- app/lib/onionbot/logger.rb
+++
@@ -1,5 +0,0 @@
-module Onionbot
-  class Logger < Ginseng::Logger
-    include Package
-  end
-end
