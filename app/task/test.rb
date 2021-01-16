f7089fcf8ef32e367aeed332eb65aa76bad484fd
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-01-17 07:02:05 +0900

    #136

--- app/task/test.rb
+++ app/task/test.rb
@@ -2,7 +2,7 @@ desc 'test all'
 task :test do
   ENV['TEST'] = Onionbot::Package.name
   require 'test/unit'
-  Dir.glob(File.join(Onionbot::Environment.dir, 'test/*.rb')).sort.each do |t|
+  Dir.glob(File.join(Onionbot::Environment.dir, 'test/*.rb')).each do |t|
     require t
   end
 end
